dad0c1dd503f843f307889dd92107f767f54bed3
llSOLOll <84673201+llSOLOll@users.noreply.github.com>
2021-06-19 16:33:25 +0530

    Add files via upload

--- insert.sql
+++ insert.sql
@@ -11,21 +11,21 @@ insert into publisher(name, country) values("Del Rey", "UK");
 insert into publisher(name, country) values("Vintage", "UK");
 
 
-insert into books(title, publisher) values ('The C Programming Language',1);
+insert into books(title, publisher) values ('The C Programming Language',(select id from publisher where name = 'PHI'));
 
-insert into books(title, publisher) values ('The Go Programming Language',1);
+insert into books(title, publisher) values ('The Go Programming Language',(select id from publisher where name = 'PHI'));
 
-insert into books(title, publisher) values ('The UNIX Programming Environment',1);
+insert into books(title, publisher) values ('The UNIX Programming Environment',(select id from publisher where name = 'PHI'));
 
-insert into books(title, publisher) values ('Cryptonomicon',2);
+insert into books(title, publisher) values ('Cryptonomicon',(select id from publisher where name = 'Harper'));
 
-insert into books(title, publisher) values ('Deep Work',3);
+insert into books(title, publisher) values ('Deep Work',(select id from publisher where name = 'GCP'));
 
-insert into books(title, publisher) values ('Atomic Habits',4);
+insert into books(title, publisher) values ('Atomic Habits',(select id from publisher where name = 'Avery'));
 
-insert into books(title, publisher) values ('The City and The City',5);
+insert into books(title, publisher) values ('The City and The City',(select id from publisher where name = 'Del Rey'));
 
-insert into books(title, publisher) values ('The Great War for Civilisation',6);
+insert into books(title, publisher) values ('The Great War for Civilisation',(select id from publisher where name = 'Vintage'));
 
 
 insert into subjects(name) values('C');
@@ -40,34 +40,34 @@ insert into subjects(name) values('Science Fiction');
 
 insert into subjects(name) values('Productivity');
 
-insert into subjects(name) values('Psycology');
+insert into subjects(name) values('Psychology');
 
 insert into subjects(name) values('Politics');
 
 insert into subjects(name) values('History');
 
 
-insert into books_subjects values(1,1);
-insert into books_subjects values(1,2);
-insert into books_subjects values(1,3);
+insert into books_subjects values((select id from books where title = 'The C Programming Language'),(select id from subjects where name = 'C'));
+insert into books_subjects values((select id from books where title = 'The C Programming Language'),(select id from subjects where name = 'UNIX'));
+insert into books_subjects values((select id from books where title = 'The C Programming Language'),(select id from subjects where name = 'Technology'));
 
-insert into books_subjects values(2,4);
-insert into books_subjects values(2,3);
+insert into books_subjects values((select id from books where title = 'The Go Programming Language'),(select id from subjects where name = 'Go'));
+insert into books_subjects values((select id from books where title = 'The Go Programming Language'),(select id from subjects where name = 'Technology'));
 
-insert into books_subjects values(3,2);
-insert into books_subjects values(3,3);
+insert into books_subjects values((select id from books where title = 'The UNIX Programming Environment'),(select id from subjects where name = 'UNIX'));
+insert into books_subjects values((select id from books where title = 'The UNIX Programming Environment'),(select id from subjects where name = 'Technology'));
 
-insert into books_subjects values(4,3);
-insert into books_subjects values(4,5);
+insert into books_subjects values((select id from books where title = 'Cryptonomicon'),(select id from subjects where name = 'Technology'));
+insert into books_subjects values((select id from books where title = 'Cryptonomicon'),(select id from subjects where name = 'Science Fiction'));
 
-insert into books_subjects values(5,3);
-insert into books_subjects values(5,6);
+insert into books_subjects values((select id from books where title = 'Deep Work'),(select id from subjects where name = 'Technology'));
+insert into books_subjects values((select id from books where title = 'Deep Work'),(select id from subjects where name = 'Productivity'));
 
-insert into books_subjects values(6,6);
-insert into books_subjects values(6,7);
+insert into books_subjects values((select id from books where title = 'Atomic Habits'),(select id from subjects where name = 'Productivity'));
+insert into books_subjects values((select id from books where title = 'Atomic Habits'),(select id from subjects where name = 'Psychology'));
 
-insert into books_subjects values(7,5);
-insert into books_subjects values(7,8);
+insert into books_subjects values((select id from books where title = 'The City and The City'),(select id from subjects where name = 'Science Fiction'));
+insert into books_subjects values((select id from books where title = 'The City and The City'),(select id from subjects where name = 'Politics'));
 
-insert into books_subjects values(8,8);
-insert into books_subjects values(8,9);
+insert into books_subjects values((select id from books where title = 'The Great War for Civilisation'),(select id from subjects where name = 'Politics'));
+insert into books_subjects values((select id from books where title = 'The Great War for Civilisation'),(select id from subjects where name = 'History'));
